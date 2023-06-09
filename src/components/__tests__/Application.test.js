45fa995f177564d574d4c7a9cf130f17403cc45a
Ekaterina <yatakshilina@gmail.com>
2023-06-09 10:37:24 -0400

    make the test async/add waitForElement to return a promise

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -3,9 +3,15 @@ import React from "react";
 import { render, cleanup } from "@testing-library/react";
 
 import Application from "components/Application";
+import {waitForElement, fireEvent} from "@testing-library/react";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  const { getByText } = render(<Application />);
+//The argument we pass to waitForElement is a function that returns a promise a DOM node.It is looking for something based on the text "Monday".
+  return waitForElement(() => getByText("Monday")).then(() => {
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
 });
